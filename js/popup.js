44e09ff5467b8e5e2db0ce61fe71a2610ed6c373
Duymq <maiduy0311@gmail.com>
2022-07-02 17:31:38 +0700

    first commit

---
+++ js/popup.js
@@ -0,0 +1,14 @@
+Swal.fire({
+    title: '',
+    width: 600,
+    padding: '3em',
+    backgroundColor: '##ef6a8a',
+    backdrop: `
+      rgba(0,0,123,0.4)
+      url("/images/nyan-cat.gif")
+      left top
+      no-repeat
+    `,
+    confirmButtonText:
+    '<i class="fa fa-thumbs-up"></i> Great!',
+  })
